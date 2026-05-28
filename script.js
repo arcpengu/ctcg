d3f4fcf9a66ba701c925a3d70e05ef85e82c53c4
Pengu <penguperson@hotmail.com>
2026-05-27 23:15:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const supabaseUrl = 'https://qqlpymxmtcmvcdiihkek.supabase.co'
+const supabaseKey = 'sb_publishable_lldulKMwPlNEYfQfW_pW6w_t5NEzOfo'
+
+const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey)
+
 document.addEventListener('DOMContentLoaded', () => {
     console.log("script.js is successfully connected!");
+    console.log("Supabase client is ready:", supabaseClient);
 });
